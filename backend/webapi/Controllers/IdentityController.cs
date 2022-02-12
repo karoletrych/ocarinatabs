9ec6c219a536f6891f4ba5faaa9fd2a58288ee5d
Karol Etrych <karol.etrych@gmail.com>
2022-02-12 20:07:49 +0100

    add identity server. fix dockerfile template to build and run image.

---
+++ backend/webapi/Controllers/IdentityController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("identity")]
+[Authorize]
+public class IdentityController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return new JsonResult(new {AAA= "helllo"});
+        return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+    }
+}
--- backend/webapi/Program.cs
+++ backend/webapi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,26 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:5001";
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ReaderScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "ocarinatabs-reader");
+    });
+});
 
 var app = builder.Build();
 
@@ -18,7 +40,15 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers()
+        .RequireAuthorization("ReaderScope");
+});
 
 app.MapControllers();
 
